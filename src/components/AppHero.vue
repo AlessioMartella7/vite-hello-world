469d2a440fcdf5ff45beba5d50df88e3da03e07b
AlessioMartella7 <alessio.mart87@gmail.com>
2024-09-13 15:53:26 +0200

    esercizio terminato

---
+++ src/components/AppHero.vue
@@ -0,0 +1,23 @@
+<script>
+    export default {
+        data(){
+            return {
+                
+            }
+        }
+    }
+
+</script>
+
+<template>
+<div class="jumbotron"></div>
+</template>
+
+<style scoped>
+.jumbotron {
+    height: 600px;
+    max-width: 1200px;
+    margin: 0 auto;
+    background-color: cadetblue;
+}
+</style>
